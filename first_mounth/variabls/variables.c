b286df8d56b60f861c9454dcf02858b5cf54cd9e
yosefther <yousefthair9@gmail.com>
2025-02-27 23:41:52 +0300

    format , add float double and long

--- first_mounth/variabls/variables.c
+++ first_mounth/variabls/variables.c
@@ -1,35 +1,36 @@
-
 #include <stdio.h>
 #include <limits.h>
+
 int main() {
-//overviwe   
-int a;
-int age = 25;          // Integer variable
-float height = 56.922;    // Floating-point variable
-char* grade = "A";      // Character variable
-double pi = 3.14159;   // Double precision floating-point variable
-printf("%d\n",age);
-printf("Age: %d\n", age); // %d used for print decimsl value
-printf("Height: %.1f feet\n", height);
-printf("Grade: %c\n", grade);
-printf("Value of Pi: %.5lf\n", pi);
+    // Integer
+    unsigned int var = UINT_MAX;  // 4 bytes
+    int var2 = INT_MIN;           // 4 bytes
+    int var3 = INT_MAX;           // 4 bytes
+    printf("Max unsigned: %u \nMin: %d\nMax: %d\n", var, var2, var3);  // %u for unsigned decimal value
 
+    unsigned short int var4 = USHRT_MAX;  // 2 bytes
+    short int var5 = SHRT_MAX;            // 2 bytes
+    short int var6 = SHRT_MIN;            // 2 bytes
+    printf("\nMax unsigned: %u \nMin: %d\nMax: %d\n", var4, var5, var6);
 
-//integer 
-unsigned int var = UINT_MAX ; //u 4 bit
-int var2  = INT_MIN ; //  4 bit 
-int var3 = INT_MAX ; // 4 bit
-printf ( "max unsigned : %u \nmin : %d\nMax : %d ",var ,var2 ,var3); // %u used for print unsigned decimal value 
+    // Character
+    char var7 = 0x7B;  // Hexadecimal format
+    char var8 = 123;   // Decimal format
+    printf("\nWritten with hex format: %c \nWritten with decimal format: %c\n", var7, var8);
 
-unsigned short int var4  = USHRT_MAX; //u2 bit 
-short int var5  = SHRT_MAX; // 2 bit 
-short int var6  = SHRT_MIN; // 2 bit
-printf ( "\nmax unsigned : %u \nmin : %d\nMax : %d ",var4 ,var5 ,var6);
+    // Float => 32 bits, Double => 64 bits, Long Double => 96 bits
+    // **Note**: The size depends on the system
+    float var9 = 3.12123444551245;
+    double var10 = 3.12384235831234;
+    long double var11 = 3.1243144356224633;
 
-//character
-char var7 = 0x7B ; //hexadecimal format
-char var8 = 123 ; //decimal format 
-printf("\nwriten with hex format :%c \nwriten with decimal format :%c ",var7,var8);
+    printf("%zu\n", sizeof(float));        // Use %zu for size_t
+    printf("%zu\n", sizeof(double));       // Use %zu for size_t
+    printf("%zu\n", sizeof(long double));  // Use %zu for size_t
 
-return 0;
+    printf("%.16f\n", var9);              // Print float with 16 decimal places
+    printf("%.16f\n", var10);             // Print double with 16 decimal places
+    printf("%.21Lf\n", var11);            // Print long double with 21 decimal places
+
+    return 0;
 }
