0b8106d20922db387710f87901a9926cd53bb2b9
Fnt <contact@test.io>
2025-03-08 16:11:51 +0300

    file1.h (Header for file1.c)

---
+++ variables/static_example/file1.h
@@ -0,0 +1,7 @@
+#ifndef FILE1_H
+#define FILE1_H
+
+void modifyStaticGlobal();  // Public function to modify static global var
+void callStaticFunction();  // Public function to call a static function
+
+#endif
